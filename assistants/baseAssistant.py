5a43d059733d3453cbb811f3b8b156c166decdb2
Estefania <estefysc@gmail.com>
2025-02-04 18:16:04 -0500

    refactoring

--- assistants/baseAssistant.py
+++ assistants/baseAssistant.py
@@ -7,89 +7,82 @@ import configparser
 from abc import ABC, abstractmethod
 from openai import OpenAI
 
-# api_key = os.environ.get("OPENAI_API_KEY")
- # Defaults to os.environ.get("OPENAI_API_KEY")
-# client = OpenAI()
-
 class BaseAssistant(ABC):
     def __init__(self, name, model="gpt-4-1106-preview"):
-        # function_json = {
-        #     "name": "display_quiz",
-        #     "description": "Displays a quiz to the student, and returns the student's response. A single quiz can have multiple questions.",
-        #     "parameters": {
-        #         "type": "object",
-        #         "properties": {
-        #             "title": {"type": "string"},
-        #             "questions": {
-        #                 "type": "array",
-        #                 "description": "An array of questions, each with a title and potentially options (if multiple choice).",
-        #                 "items": {
-        #                     "type": "object",
-        #                     "properties": {
-        #                         "question_text": {"type": "string"},
-        #                         "question_type": {
-        #                             "type": "string",
-        #                             "enum": ["MULTIPLE_CHOICE", "FREE_RESPONSE"],
-        #                         },
-        #                         "choices": {"type": "array", "items": {"type": "string"}},
-        #                     },
-        #                     "required": ["question_text"],
-        #                 },
-        #             },
-        #         },
-        #         "required": ["title", "questions"],
-        #     },
-        # }
+        # Note: To see the complete structure of a tool function, see the notes.md file.
         get_summary_json = {
             "name": "get_summary",
             "description": "Obtains a summary of the user's unread emails. Returns a string. The string will contain the email address of the sender and how many emails from this sender are not read.",
             "parameters": { "type": "object", "properties": {}, "required": [] }
         }
+        get_generic_json = {
+            "name": "get_generic",
+            "description": "Generic response to the user's message.",
+            "parameters": { "type": "object", "properties": {}, "required": [] }
+        }
+
         config = configparser.ConfigParser()
         config.read('config.cfg')
-        # self.api_key = os.environ.get("OPENAI_API_KEY")
+        
         self.api_key = config['OpenAI']['OPENAI_API_KEY']
         self.client = OpenAI(api_key=self.api_key)
         self.name = name
         self.model = model
-        self.tools = [{"type": "code_interpreter"}, {"type": "function", "function": get_summary_json}]
+        # self.tools = [{"type": "code_interpreter"}, {"type": "function", "function": get_summary_json}]
+        self.tools = [
+            # {"type": "code_interpreter"}, 
+            {"type": "function", "function": get_summary_json},
+            {"type": "function", "function": get_generic_json}
+        ]
 
     def _createAssistant(self, instruction):
-        print("Creating assistant from BaseAssistant._createAssistant")
+        print("Creating assistant from BaseAssistant._createAssistant\n")
         assistant = self.client.beta.assistants.create(
             name = self.name,
             instructions = instruction,
             tools = self.tools,
             model = self.model
         )
+        print("from baseAssistant._createAssistant: This is the assistantID: ", assistant.id, "\n")
         return assistant.id
 
-    def _createThread(self, userMessage):
+    # def _createThread(self, userMessage):
+    def _createThread(self):
         thread = self.client.beta.threads.create()
-        message = self.client.beta.threads.messages.create(
-            thread_id = thread.id,
-            role="user",
-            content = userMessage
-        )
+        # message = self.client.beta.threads.messages.create(
+        #     thread_id = thread.id,
+        #     role="user",
+        #     content = userMessage
+        # )
         return thread.id
     
-    def _modifyThread(self, threadId, userMessage):
+    # def _modifyThread(self, threadId, userMessage):
+    #     message = self.client.beta.threads.messages.create(
+    #         thread_id = threadId,
+    #         role="user",
+    #         content = userMessage
+    #     )
+    #     return threadId
+
+    def _addMessageToThread(self, threadId, userMessage):
         message = self.client.beta.threads.messages.create(
             thread_id = threadId,
             role="user",
             content = userMessage
         )
+        print("from baseAssistant._addMessageToThread: This is the message: ", message, "\n")
         return threadId
 
     def _triggerAssistant(self, threadId, assistantId):
         run = self.client.beta.threads.runs.create(
-        thread_id = threadId,
-        assistant_id = assistantId
+            thread_id = threadId,
+            assistant_id = assistantId
         )
+        print("from baseAssistant._triggerAssistant: This is the run: ", run, "\n")
         return run
 
     def _submitToolOutputs(self, threadId, runId, toolCallId, responses):
-        print("Submitting tool outputs")
+        print("Submitting tool outputs\n")
         run = self.client.beta.threads.runs.submit_tool_outputs(
             thread_id=threadId,
             run_id=runId,
@@ -103,24 +96,35 @@ class BaseAssistant(ABC):
         # print("Run after submitting tool outputs: ", run)
         return run
 
-    def _waitForRun(self, threadId, run):
-        print("Waiting for run")
-        # print("This is the run: ", run)
-        # print("This is the run status: ", run.status)
+    def _waitForRun(self, threadId, run, timeout=300):
+        # Returns the run object when its status is not queued or in_progress
+        
+        print("Waiting for run\n")
+        start_time = time.time()
+        print("From baseAssistant._waitForRun: This is the run before waiting: ", run, "\n")
         while run.status == "queued" or run.status == "in_progress":
+            # Check for timeout
+            if time.time() - start_time > timeout:
+                raise TimeoutError(f"From baseAssistant._waitForRun: Run timed out after {timeout} seconds")
+            
+            time.sleep(1)
+            
             run = self.client.beta.threads.runs.retrieve(
-            thread_id = threadId,
-            run_id = run.id
+                thread_id = threadId,
+                run_id = run.id
             )
-        time.sleep(0.5)
-        # print("Run after waiting: ", run)
+            print("From baseAssistant._waitForRun: This is the run after retrieving: ", run, "\n")
+        print("From baseAssistant._waitForRun: Run after waiting: ", run, "\n")
+        
         return run
 
     def _getAssistantResponse(self, threadId):
-        # Display the Assistant's Response
+        # NOTE: When the assitant responds with the user message, the assistant_id
+        # in the message object is None.
         messages = self.client.beta.threads.messages.list(
-        thread_id = threadId
+            thread_id = threadId
         )
+        print("from baseAssistant._getAssistantResponse: This is the messages: ", messages, "\n")
         response = messages.data[0].content[0].text.value
         return response
     
@@ -132,7 +136,7 @@ class BaseAssistant(ABC):
         """Delete an assistant by ID."""
         HEADERS = {
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {self.api_key}",  # Replace with your actual API key
+            "Authorization": f"Bearer {self.api_key}",  
             "OpenAI-Beta": "assistants=v1"
         }
         delete_url = f"https://api.openai.com/v1/assistants/{assistant_id}"
